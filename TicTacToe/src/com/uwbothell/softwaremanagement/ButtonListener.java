367313ebe8949dc01bf3f5c2b1426c8fe7f1ddf4
Sirisha Satyavada <svnsirisha@gmail.com>
2020-11-05 21:18:47 -0800

    For every button click, place X or O depending upon the player turn

--- TicTacToe/src/com/uwbothell/softwaremanagement/ButtonListener.java
+++ TicTacToe/src/com/uwbothell/softwaremanagement/ButtonListener.java
@@ -1,10 +1,25 @@
 package com.uwbothell.softwaremanagement;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ButtonListener implements ActionListener {
-    public void actionPerformed(ActionEvent e) {
+    private static int TURN_COUNT = 0;
+    public void actionPerformed(ActionEvent event) {
+        JButton button = (JButton)event.getSource();
+        if (TURN_COUNT %2 == 0) {
+            button.setText("X");
+        }
+        else {
+            button.setText("O");
+        }
 
+        // TODO
+        // After every click check if some player actually won the game (checkWinningCriteria() )
+        // If some player won, then say game is over and reset board (endGame(), resetBoard())
+
+        // Else, increment the count of turns and wait for next click
+        TURN_COUNT ++;
     }
 }
